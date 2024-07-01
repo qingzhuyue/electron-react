00c2288d5edc30e9fa83012a0d79a0156a65b81a
qingzhuyue <qingzhuyue@foxmail.com>
2024-07-01 20:47:37 +0800

    修改了镜像源

--- electron-main/main.js
+++ electron-main/main.js
@@ -2,8 +2,8 @@
  * @Author: qingzhuyue qingzhuyue@foxmail.com
  * @Date: 2024-01-30 15:36:34
  * @LastEditors: qingzhuyue qingzhuyue@foxmail.com
- * @LastEditTime: 2024-01-30 15:54:22
- * @FilePath: /vite-electron-react/electron/main.js
+ * @LastEditTime: 2024-07-01 20:42:50
+ * @FilePath: /vite-electron-react/electron-main/main.js
  * @Description: 
  * Copyright (c) 2024 by ${qingzhuyue} email: ${qingzhuyue@foxmail.com}, All Rights Reserved.
  */
@@ -27,12 +27,6 @@ async function createWindow() {
       mainWindow?.webContents.openDevTools();
     }
   });
-
-  // const { RENDERER_DEV_SERVER_URL } = process.env;
-
-  // const pageUrl = NODE_ENV === 'development' && RENDERER_DEV_SERVER_URL
-  //   ? RENDERER_DEV_SERVER_URL
-  //   : new URL(join(__dirname, '../../renderer/build/index.html'), `file://${__dirname}`).toString();
   const pageUrl = NODE_ENV === 'development'? 'http://localhost:5173': `file://${path.join(__dirname, '../dist/index.html')}`
 
   await mainWindow.loadURL(pageUrl);
